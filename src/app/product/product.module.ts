04870bbcbdf7fc01bdbfbd16768ad09b9fcd3edb
kunoandpandapi <wkawato@gmail.com>
2020-08-13 05:31:09 +0900

    Angular FrontEnd側作成

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListingsComponent } from './product-listings/product-listings.component';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+
+export const routes: Routes = [
+
+  {
+    path: 'products', component: ProductComponent,
+    children: [
+      {path: '', component: ProductListingsComponent},
+      {path: ':productId', component: ProductDetailComponent}
+    ]
+  }
+];
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductDetailComponent,
+    ProductListingsComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+})
+export class ProductModule { }
