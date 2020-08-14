ffc0e8a32fe53d0d40e41fd42bb0e0f019406579
kunoandpandapi <wkawato@gmail.com>
2020-08-14 09:03:00 +0900

    Nodeサーバー構築

---
+++ server/model/product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductsSchema = new Schema({
+  image: String,
+  name: { type: String, required: true, max:[60, '最大60文字まで']},
+  price: Number,
+  heading1: String,
+  heading2: String,
+  heading3: String,
+  headingtxt1: String,
+  headingtxt2: String,
+  headingtxt3: String
+});
+
+module.exports = mongoose.model('Product', ProductsSchema)
